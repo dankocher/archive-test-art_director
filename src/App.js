457ffda5fb979daef1e3f3422122fb1cc28a2e4c
KarachunVladislav01 <karachunvladislav@gmail.com>
2020-08-26 12:40:55 +0300

    feat: added structure and styling of DeviceError component. fix: some css optimization

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import  Authorization from "./Components/Authorization/Authorization"
-import WelcomePage from "./Components/WelcomePage/WelcomePage"
+import DeviceError from "./Components/DeviceError/DeviceError";
+import Authorization from "./Components/Authorization/Authorization";
+import WelcomePage from "./Components/WelcomePage/WelcomePage";
 
 function App() {
-  return (
-    <div>
-      <WelcomePage />
-    </div>
-  );
+	return <WelcomePage />;
 }
 
 export default App;
--- src/Components/DeviceError/DeviceError.js
+++ src/Components/DeviceError/DeviceError.js
@@ -1,9 +1,18 @@
-import React from "react"
+import React from "react";
+import "./DeviceError.css"
 
 function DeviceError() {
-    return (
-        <div></div>
-    );
+	return (
+		<div className="centered-container--deviceError">
+			<div>
+				<h1 className="header--deviceError">Oops!</h1>
+				<p className="error--deviceError">
+					Сайт не поддерживает ваше устройство. Попробуйте зайти с другого
+					устройства.
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export default DeviceError;
